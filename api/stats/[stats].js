bf3aff66541af38ef89814e18df7ce917a995b2c
hx-w <herixth@163.com>
2021-06-05 16:00:18 +0800

    name fix

--- api/stats/[stats].js
+++ api/stats/[stats].js
@@ -1,7 +1,6 @@
 const HLTV = require('hltv-api').default
 
 module.exports = async(req, res) => {
- // const stats = await HLTV.getStatsByMatchId(req.params)
- // res.json(stats)
-  res.send(`hello ${req.query.stats}`)
+  const stats = await HLTV.getStatsByMatchId(req.query.stats)
+  res.json(stats)
 }
