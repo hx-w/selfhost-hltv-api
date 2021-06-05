d3e2d0df63255429418c8f70eb10f3b4f5903f90
hx-w <herixth@163.com>
2021-06-05 15:42:04 +0800

    name test

--- api/stats/stats.js
+++ api/stats/stats.js
@@ -3,8 +3,5 @@ const HLTV = require('hltv-api').default
 module.exports = async(req, res) => {
  // const stats = await HLTV.getStatsByMatchId(req.params)
  // res.json(stats)
-  const {
-    query: { name },
-  } = req
-  res.send(`hello ${name}`)
+  res.send(`hello ${req.query.name}`)
 }
